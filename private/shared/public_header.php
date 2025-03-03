025748fe3ab6393f949d4debd6284dcc26b1033d
Olly Silinski <oasilins@gmail.com>
2025-03-02 19:39:05 -0500

    Some CSS figured out

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -1,36 +1,42 @@
 <?php
-  if(!isset($page_title)) { $page_title = 'WandrBae'; }
+if (!isset($page_title)) {
+  $page_title = 'WandrBae';
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Wandrbae<?php echo h($page_title); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
-    </script>
-    <script src="<?php echo url_for('/js/route_map.js'); ?>"></script>
-    
-  </head>
-  <body>
-    <header id="page-header" role="banner" aria-label="document-header">
-      <div>
+
+<head>
+  <meta charset="utf-8">
+  <title>Wandrbae<?php echo h($page_title); ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/public.css'); ?>" />
+  </script>
+  <script src="<?php echo url_for('/js/route_map.js'); ?>"></script>
+
+</head>
+
+<body>
+  <header id="page-header" role="banner" aria-label="document-header">
+    <div class="header-content">
+      <div class="header-text">
         <h1>Wandrbae</h1>
         <p>Wander with us, bae.</p>
         <p>PUBLIC HEADER ACTIVE</p>
-        <div class="header-buttons">
-          <a href="#" class="btn">Join Us</a>
-          <a href="#" class="btn">Log In</a>
-        </div>
+
       </div>
-    </header>
-    <nav role="navigation">
-      <ul>
-        <li><a href="<?php echo url_for('/public/members/routes/index.php'); ?>">Routes</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Join Us</a></li>
-        <li><a href="#">Contact Us</a></li>
-      </ul>
-    </nav>
-  
+      <div class="header-buttons">
+        <a href="#" class="btn">Join Us</a>
+        <a href="#" class="btn">Log In</a>
+      </div>
+    </div>
+  </header>
+  <nav role="navigation">
+    <ul>
+      <li><a href="<?php echo url_for('/public/members/routes/index.php'); ?>">Routes</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Join Us</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </nav>
